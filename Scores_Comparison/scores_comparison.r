ba03e544e1e67fc9766e217495cec2e3718a2c74
jnghlim <46730344+jnghlim@users.noreply.github.com>
2021-10-29 22:10:57 -0700

    Score Comparisons

--- Scores_Comparison/scores_comparison.r
+++ Scores_Comparison/scores_comparison.r
@@ -44,3 +44,87 @@ plot3 <- ggplot(data=ploe_meanscore_df, aes(x=group, y=reading)) + geom_bar(stat
 grid.arrange(plot1,plot2,plot3)
 ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Scores_Comparison\\parental_level.png"), arrangeGrob(plot1, plot2, plot3))
 dev.off()
+
+colnames(sdata)
+#Scores Comparison by level of lunch
+unique(sdata$lunch)
+l_list <- c("standard", "free/reduced")
+l_math_mean <- l_wr_mean <- l_rd_mean <- NULL
+
+mean(sdata$math.percentage[sdata$lunch == "standard"])
+mean(sdata$writing.score.percentage[sdata$lunch == "free/reduced"])
+
+for (x in l_list){
+    l_math_mean <- append(l_math_mean, mean(sdata$math.percentage[sdata$lunch == x]))
+    l_wr_mean <- append(l_wr_mean, mean(sdata$writing.score.percentage[sdata$lunch == x]))
+    l_rd_mean <- append(l_rd_mean, mean(sdata$reading.score.percentage[sdata$lunch == x]))
+}
+l_math_mean
+l_wr_mean
+l_rd_mean
+
+l_meanscore_df <- data.frame(math = l_math_mean, writing=l_wr_mean, reading=l_rd_mean, group = c("Standard", "Free/Reduced"))
+l_meanscore_df
+require(gridExtra)
+plot1 <- ggplot(data=l_meanscore_df, aes(x=group, y=math)) + geom_bar(stat='identity')
+plot2 <- ggplot(data=l_meanscore_df, aes(x=group, y=writing)) + geom_bar(stat='identity')
+plot3 <- ggplot(data=l_meanscore_df, aes(x=group, y=reading)) + geom_bar(stat='identity')
+grid.arrange(plot1,plot2,plot3)
+ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Scores_Comparison\\lunch.png"), arrangeGrob(plot1, plot2, plot3))
+dev.off()
+#Scores Comparison by level of test_preparation
+unique(sdata$test.preparation.course)
+
+t_list <- c("none", "completed")
+t_math_mean <- t_wr_mean <- t_rd_mean <- NULL
+
+mean(sdata$math.percentage[sdata$test.preparation.course == "none"])
+mean(sdata$writing.score.percentage[sdata$test.preparation.course == "completed"])
+
+for (x in t_list){
+    t_math_mean <- append(t_math_mean, mean(sdata$math.percentage[sdata$test.preparation.course == x]))
+    t_wr_mean <- append(t_wr_mean, mean(sdata$writing.score.percentage[sdata$test.preparation.course == x]))
+    t_rd_mean <- append(t_rd_mean, mean(sdata$reading.score.percentage[sdata$test.preparation.course == x]))
+}
+t_math_mean
+t_wr_mean
+t_rd_mean
+
+t_meanscore_df <- data.frame(math = t_math_mean, writing= t_wr_mean, reading= t_rd_mean, group = c("None", "Completed"))
+t_meanscore_df
+
+require(gridExtra)
+plot1 <- ggplot(data=t_meanscore_df, aes(x=group, y=math)) + geom_bar(stat='identity')
+plot2 <- ggplot(data=t_meanscore_df, aes(x=group, y=writing)) + geom_bar(stat='identity')
+plot3 <- ggplot(data=t_meanscore_df, aes(x=group, y=reading)) + geom_bar(stat='identity')
+grid.arrange(plot1,plot2,plot3)
+ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Scores_Comparison\\test_preparation.png"), arrangeGrob(plot1, plot2, plot3))
+dev.off()
+#Scores Comparison by level of sex
+unique(sdata$sex)
+
+g_list <- c("F", "M")
+g_math_mean <- g_wr_mean <- g_rd_mean <- NULL
+
+mean(sdata$math.percentage[sdata$sex == "F"])
+mean(sdata$writing.score.percentage[sdata$sex == "M"])
+
+for (x in g_list){
+    g_math_mean <- append(g_math_mean, mean(sdata$math.percentage[sdata$sex == x]))
+    g_wr_mean <- append(g_wr_mean, mean(sdata$writing.score.percentage[sdata$sex == x]))
+    g_rd_mean <- append(g_rd_mean, mean(sdata$reading.score.percentage[sdata$sex == x]))
+}
+g_math_mean
+g_wr_mean
+g_rd_mean
+
+g_meanscore_df <- data.frame(math = g_math_mean, writing= g_wr_mean, reading= g_rd_mean, group = c("F", "M"))
+g_meanscore_df
+
+require(gridExtra)
+plot1 <- ggplot(data=g_meanscore_df, aes(x=group, y=math)) + geom_bar(stat='identity')
+plot2 <- ggplot(data=g_meanscore_df, aes(x=group, y=writing)) + geom_bar(stat='identity')
+plot3 <- ggplot(data=g_meanscore_df, aes(x=group, y=reading)) + geom_bar(stat='identity')
+grid.arrange(plot1,plot2,plot3)
+ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Scores_Comparison\\gender.png"), arrangeGrob(plot1, plot2, plot3))
+dev.off()
