c96c9bc66c05096825448d199e28abc7b6cba92a
jnghlim <46730344+jnghlim@users.noreply.github.com>
2021-10-31 15:12:27 -0700

    Create main.r

---
+++ main.r
@@ -0,0 +1,10 @@
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("gridExtra")
+
+library("ggplot2")
+library("dplyr")
+library("gridExtra")
+
+sdata <- read.csv("Student Performance New.csv")
+as_tibble(sdata)
