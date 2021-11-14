680f4f9cdcc115c6be87eca54b711b566740c9f9
jnghlim <46730344+jnghlim@users.noreply.github.com>
2021-11-13 23:38:51 -0800

    Changes

--- main.r
+++ main.r
@@ -6,7 +6,7 @@ as_tibble(sdata)
 names(sdata)
 
 cat_var <- names(sdata)[which(sapply(sdata, is.character))]
-cat_var
 num_var <- names(sdata)[which(sapply(sdata, is.numeric))]
 num_var <- num_var[num_var != "X"]
 num_var
+cat_var
--- outliers.r
+++ outliers.r
@@ -1,24 +1,29 @@
 for (x in num_var){
-    q1 = quantile(sdata$math.percentage, 0.25, interpolation="nearest")
-    q2 = quantile(sdata$math.percentage, 0.5, interpolation="nearest")
-    q3 = quantile(sdata$math.percentage, 0.75, interpolation="nearest")
-    q4 = quantile(sdata$math.percentage, 1, interpolation="nearest")
+    q1 = quantile(unlist(sdata[x], use.names=FALSE), 0.25, interpolation="nearest")
+    q2 = quantile(unlist(sdata[x], use.names=FALSE), 0.5, interpolation="nearest")
+    q3 = quantile(unlist(sdata[x], use.names=FALSE), 0.75, interpolation="nearest")
+    q4 = quantile(unlist(sdata[x], use.names=FALSE), 1, interpolation="nearest")
     IQR = q3 - q1
     LB = q1 - 1.5*IQR
     UB = q3 + 1.5*IQR
     if ((min(sdata[x]) < LB & max(sdata[x]) > UB)){
         print("No Outliers")
     } else {
-        print("There are Outliers")
+        print(glue("There are Outliers in {x}"))
         print(glue("Lower Bound is: {LB}"))
         print(glue("Upper Bound is: {UB}"))
         print(sdata[sdata[x] < LB, x])
         print(sdata[sdata[x] > UB, x])
     }
 }
-
 ### Boxplots
 box_data <- sdata %>% select(math.percentage, reading.score.percentage, writing.score.percentage)
 jpeg("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Scores\\scores_box_plot.jpeg", quality = 75)
 boxplot(box_data)
 dev.off()
+
+for (x in num_var) {
+    print(quantile(sdata[sdata[x]], 0.25, interpolation="nearest"))
+}
+unlist(sdata["math.percentage"], use.names=FALSE)
+typeof(sdata$math.percentage)
