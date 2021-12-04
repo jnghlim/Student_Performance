a72107bd2f5ceba076391ec5cfbbfe15a434cb10
jnghlim <46730344+jnghlim@users.noreply.github.com>
2021-12-03 22:34:08 -0800

    Update dist.r

--- Distribution/dist.r
+++ Distribution/dist.r
@@ -16,13 +16,13 @@ plot5 <- ggplot(sdata, aes(x = sdata[,"test.preparation.course"])) + geom_bar() 
 grid.arrange(plot1,plot2,plot3,plot4, plot5)
 ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Distribution\\bar_dist.png"), arrangeGrob(plot1, plot2, plot3, plot4, plot5))
 dev.off()
-
+plot2
 num_data <- sdata[num_var]
 
 require(gridExtra)
-hist1 <- ggplot(num_data, aes(x=math.percentage)) + geom_histogram()
-hist2 <- ggplot(num_data, aes(x=writing.score.percentage)) + geom_histogram()
-hist3 <- ggplot(num_data, aes(x=reading.score.percentage)) + geom_histogram()
+hist1 <- ggplot(num_data, aes(x=math.percentage, fill="#8468EA")) + geom_histogram()
+hist2 <- ggplot(num_data, aes(x=writing.score.percentage, fill="#A8EA68")) + geom_histogram()
+hist3 <- ggplot(num_data, aes(x=reading.score.percentage, fill="#EAC268")) + geom_histogram()
 grid.arrange(hist1, hist2, hist3)
 ggsave(glue("C:\\Users\\lim95\\Documents\\GitHub\\student_performance_r_project\\Distribution\\score_dist.png"), arrangeGrob(hist1, hist2, hist3))
 dev.off()
